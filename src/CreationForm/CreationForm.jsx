41f47cc0dd846d071aacba0489b2dc09a4a7bfbe
git01bit <majidpour018@gmail.com>
2025-08-22 13:28:29 +0330

    feat(CreationForm): implement save data to project object functionality

--- src/CreationForm/CreationForm.jsx
+++ src/CreationForm/CreationForm.jsx
@@ -17,19 +17,54 @@ function CreationForm({ project, setProject }) {
     setId((i) => i + 1);
   }
 
+  function saveProjectTitle(e) {
+    setProject((p) => ({ ...p, projectTitle: e.target.value }));
+  }
+
+  function saveProjectStepTitle(e, stepId) {
+    setProject((p) => ({
+      ...p,
+      steps: p.steps.map((step) =>
+        step.stepId === stepId ? { ...step, stepTitle: e.target.value } : step
+      ),
+    }));
+  }
+
+  function saveProjectStepDescription(e, stepId) {
+    setProject((p) => ({
+      ...p,
+      steps: p.steps.map((step) =>
+        step.stepId === stepId
+          ? { ...step, stepDescription: e.target.value }
+          : step
+      ),
+    }));
+  }
+  console.log(project);
+
   return (
     <section>
       <input
         type="text"
         placeholder="نام پروژه"
         className={styles.projectNameInput}
+        value={project.projectTitle}
+        onChange={(e) => saveProjectTitle(e)}
       />
 
       <div className={styles.stepsContainer}>
         {project.steps.map((step) => (
           <div key={step.stepId} className={styles.stepContainer}>
-            <input type="text" placeholder="عنوان فعالیت" />
-            <textarea rows="4" placeholder="شرح فعالیت"></textarea>
+            <input
+              type="text"
+              placeholder="عنوان فعالیت"
+              onChange={(e) => saveProjectStepTitle(e, step.stepId)}
+            />
+            <textarea
+              rows="4"
+              placeholder="شرح فعالیت"
+              onChange={(e) => saveProjectStepDescription(e, step.stepId)}
+            ></textarea>
           </div>
         ))}
 
